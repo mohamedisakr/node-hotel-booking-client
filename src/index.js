d2163328493747a4051a8d8e90b4fb0eeb4ccf97
mohamedisakr <mohamedisakr@gmail.com>
2021-12-29 08:52:19 +0200

    installing & configuring redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {createStore, combineReducers} from 'redux'
+import {Provider} from 'react-redux'
+import {composeWithDevTools} from 'redux-devtools-extension'
 import 'bootstrap/dist/css/bootstrap.css'
 import './index.css'
 import App from './App'
 
+const authReducer = (state = {}, action) => {
+  switch (action.type) {
+    case 'LOGGED_IN_USER':
+      return {...state, ...action.payload}
+    case 'LOGOUT':
+      return action.payload
+    default:
+      return state
+  }
+}
+
+const rootReducer = combineReducers({user: authReducer})
+
+const store = createStore(rootReducer, composeWithDevTools)
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 )
