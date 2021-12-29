83bcb3b301e30ecfcff0ae4a6a2fd583dab366d6
mohamedisakr <mohamedisakr@gmail.com>
2021-12-29 13:16:39 +0200

    adding register form basic structure

--- src/auth/Register.js
+++ src/auth/Register.js
@@ -1,5 +1,35 @@
+import {useState} from 'react'
+
 const Register = () => {
-  return <div className="container-fluid h1">Register Page</div>
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    alert('send user info to backend')
+  }
+
+  const registerForm = () => {
+    return (
+      <form onSubmit={handleSubmit}>
+        <button type="submit">Submit</button>
+      </form>
+    )
+  }
+
+  return (
+    <>
+      <div className="container-fluid bg-secondary p-5 text-center">
+        <h1>Register</h1>
+      </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6 offset-md-3">{registerForm()}</div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Register
