07564a8c5c8352e20f9f5bb1174e573229715b78
mohamedisakr <mohamedisakr@gmail.com>
2021-12-31 18:43:31 +0200

    implementing dashboard navigation

--- src/user/Dashboard.js
+++ src/user/Dashboard.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import DashboardNav from '../components/DashboardNav'
 
 const Dashboard = () => {
   return (
@@ -6,7 +7,9 @@ const Dashboard = () => {
       <div className="container-fluid bg-secondary p-5">
         <h1> Dashboard</h1>
       </div>
-
+      <div className="container-fluid p-4">
+        <DashboardNav />
+      </div>
       <div className="container">
         <p>Show all bookings and a button to browse hotels</p>
       </div>
