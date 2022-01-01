a6677af645907cee91538d1cf91b82d6888e6e6d
mohamedisakr <mohamedisakr@gmail.com>
2022-01-01 07:36:17 +0200

    building connect nav layout

--- src/components/ConnectNav.js
+++ src/components/ConnectNav.js
@@ -19,6 +19,16 @@ const ConnectNav = () => {
           description={`Joined ${moment(createdAt).fromNow()}`}
         />
       </Card>
+
+      {auth &&
+        auth.user &&
+        auth.user.stripe_seller &&
+        auth.user.stripe_seller.charges_enabled(
+          <>
+            <div>Pending Balance</div>
+            <div>Payout Settings</div>
+          </>,
+        )}
     </div>
   )
 }
