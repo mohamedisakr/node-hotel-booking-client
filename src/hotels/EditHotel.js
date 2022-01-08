daa619b1bff57c1521a89858a81472246c8f83d9
mohamedisakr <mohamedisakr@gmail.com>
2022-01-08 13:30:23 +0200

    creating edit hotel page

--- src/hotels/EditHotel.js
+++ src/hotels/EditHotel.js
@@ -5,13 +5,29 @@ import {read} from '../actions/hotel'
 import {toast} from 'react-toastify'
 // import HotelNewForm from '../components/HotelNewForm'
 
-const EditHotel = ({match}) => {
-  const [hotel, setHotel] = useState({})
+const EditHotel = () => {
+  // redux
   const {
     auth: {token, user},
   } = useSelector((state) => ({...state}))
 
+  // parameter
   const {hotelId} = useParams()
+
+  // state
+  const [title, setTitle] = useState('')
+  const [content, setContent] = useState('')
+  const [location, setLocation] = useState('')
+  const [image, setImage] = useState('')
+  const [price, setPrice] = useState('')
+  const [from, setFrom] = useState('')
+  const [to, setTo] = useState('')
+  const [bed, setBed] = useState('')
+  const [postedBy, setPostedBy] = useState('')
+  const [preview, setPreview] = useState(
+    `${process.env.PUBLIC_URL}/images/100-100-preview.png`,
+  )
+  const [hotel, setHotel] = useState({})
 
   useEffect(() => {
     loadSellerHotel()
@@ -23,6 +39,92 @@ const EditHotel = ({match}) => {
     } = await read(hotelId)
     console.log(data)
     setHotel(data)
+    const {title, content, location, image, price, from, to, bed, postedBy} =
+      data
+    setTitle(title)
+    setContent(content)
+    setLocation(location)
+    setImage(image)
+    setPrice(price)
+    setFrom(from)
+    setTo(to)
+    setBed(bed)
+    setPostedBy(postedBy)
+  }
+
+  const resetFormState = () => {
+    setTitle('')
+    setContent('')
+    setLocation('')
+    setImage('')
+    setPrice('')
+    setFrom('')
+    setTo('')
+    setBed('')
+    setPostedBy('')
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    setPostedBy(user._id)
+    const hotelToAdd = {
+      title,
+      content,
+      location,
+      image,
+      price,
+      from,
+      to,
+      bed,
+      postedBy: postedBy || user._id,
+    }
+
+    try {
+      //   const res = await createHotel(token, hotelToAdd)
+      //   console.log(`response : ${res}`)
+      //   // console.log(JSON.stringify(hotelToAdd, null, 4))
+      //   resetFormState()
+      //   toast.success('New hotel added successfully')
+    } catch (err) {
+      console.log(`Error : ${err}`)
+      toast.error(`${err}`)
+
+      // if (err.status === 400) {
+      //   toast.error(`${err}`)
+      //   // toast.error(`${err.message}`)
+      // }
+
+      // if (err.response.status === 400) {
+      //   toast.error(`${err.response.data.message}`)
+      // }
+    }
+  }
+
+  const handleImageChange = (event) => {
+    const files = event.target.files
+    const firstFile = files[0]
+    setImage(firstFile.name)
+    setPreview(URL.createObjectURL(firstFile))
+  }
+
+  const handleTitleChange = (event) => {
+    setTitle(event.target.value)
+  }
+
+  const handleContentChange = (event) => {
+    setContent(event.target.value)
+  }
+
+  const handleLocationChange = (event) => {
+    setLocation(event.target.value)
+  }
+
+  const handlePriceChange = (event) => {
+    setPrice(event.target.value)
+  }
+
+  const handleBedChange = (value) => {
+    setBed(value)
   }
 
   return (
@@ -30,8 +132,30 @@ const EditHotel = ({match}) => {
       <div className="container-fluid bg-secondary p-5 text-center">
         <h1>Edit hotel</h1>
       </div>
-      <div className="col-md-2">
-        <pre>{JSON.stringify({hotel}, null, 4)}</pre>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-10">show edit form</div>
+          <div className="col-md-2">
+            <img src={preview} alt="preview" className="img img-fluid m-2" />
+            <pre>
+              {JSON.stringify(
+                {
+                  postedBy,
+                  title,
+                  content,
+                  location,
+                  image,
+                  price,
+                  from,
+                  to,
+                  bed,
+                },
+                null,
+                4,
+              )}
+            </pre>
+          </div>
+        </div>
       </div>
     </>
   )
