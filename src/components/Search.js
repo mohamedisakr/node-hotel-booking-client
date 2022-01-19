54b07c201dbe29b1593af028c3ec30c0baca1eb2
mohamedisakr <mohamedisakr@gmail.com>
2022-01-19 11:41:02 +0200

    adding datepicker & beds to search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -29,17 +29,41 @@ const Search = () => {
 
   return (
     <div className="d-flex pb-4 ">
-      <div className="w-100">
-        <input
-          type="text"
-          name="location"
-          data-test-id="location"
-          className="form-control m-2"
-          placeholder="Location"
-          value={location}
-          onChange={handleLocationChange}
-        />
-      </div>
+      {/* <div className="w-100"> */}
+      <input
+        type="text"
+        name="location"
+        data-test-id="location"
+        className="form-control m-2 w-100"
+        placeholder="Location"
+        value={location}
+        onChange={handleLocationChange}
+      />
+      <RangePicker
+        onChange={(value, dateString) => setDate(dateString)}
+        disabledDate={(current) =>
+          current && current.valueOf() < moment().subtract(1, 'days')
+        }
+        className="w-100"
+        data-test-id="fromDate"
+      />
+      <Select
+        placeholder="Number of Beds"
+        data-test-id="bed"
+        onChange={(value) => setBed(value)}
+        className="w-100 m-2"
+        size="large"
+      >
+        <Option key={1}>{1}</Option>
+        <Option key={2}>{2}</Option>
+        <Option key={3}>{3}</Option>
+        <Option key={4}>{4}</Option>
+      </Select>
+
+      <SearchOutlined
+        onClick={handleSubmit}
+        className="btn btn-primary btn-square p-3"
+      />
     </div>
   )
 }
